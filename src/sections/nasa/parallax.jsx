24baf8f6acba7fdce36c00d3c217a0ceba99da9d
Luis Baptista <>
2024-11-27 21:27:07 -0800

    Added image for parallax Setup dynamic style for parallax

--- src/sections/nasa/parallax.jsx
+++ src/sections/nasa/parallax.jsx
@@ -1,6 +1,12 @@
+import featureBG from "/images/nasa/mockup.png";
+
 export const Parallax = () => {
+  const imgSrc = {
+    backgroundSize: "contain",
+    backgroundImage: `url(${featureBG})`
+  };
   return (
-    <section className="parallax-container"></section>  
+    <section className="parallax-container" style={imgSrc}></section>  
   )
 }
 

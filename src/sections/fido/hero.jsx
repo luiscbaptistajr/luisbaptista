21fe16e9c43a2e4b05c5a558416638aa7f31c2b1
Luis Baptista <>
2024-11-28 04:09:37 -0800

    Completed Fido case study

--- src/sections/fido/hero.jsx
+++ src/sections/fido/hero.jsx
@@ -8,8 +8,8 @@ export const Hero = () => {
             <h2 className="section-title-h2 inner">Ux Design Study</h2>
             <p className="body-text-p1">Fido Solutions Inc. is a Canadian mobile network operator owned by Rogers Communications. Since its acquisition by Rogers in 2004, it has operated as a Mobile virtual network operator (MVNO) using the Rogers Wireless network.</p>
             <p className="body-text-p3 pt-5">
-              <Link className="btn-green mr-4" to="/">Prototype</Link>
-              <Link className="btn-green" to="/">Website</Link>
+              <Link className="btn-green mr-4" target="_blank" to="https://www.figma.com/proto/G33S8zeT8OdcWY4PbwCVNR/FIDO-Wireframe?page-id=96%3A2040&node-id=96-2042&node-type=canvas&viewport=789%2C461%2C0.37&t=5OZSlZqWXT2HxLkm-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=96%3A2042">Prototype</Link>
+              {/* <Link className="btn-green" to="/">Website</Link> */}
             </p>
         </div>
         <div>
--- src/sections/fido/wireframes.jsx
+++ src/sections/fido/wireframes.jsx
@@ -58,13 +58,53 @@ export const Wireframes = () => {
                 <h2 className="section-title-h2">New User Flow</h2>
                 {/* <p className="body-text-p1">Together, these colors create a balanced yet dynamic palette that communicates trust and professionalism (through blue tones) and boldness or passion (through red). This combination reflects qualities often associated with space exploration: intelligence, reliability, and the adventurous spirit of discovery.</p> */}
                 <ul className="item-list">
+                  <li><span className="annotation">A</span>With the new user flow, I've made the page content more minimalist, allowing the design and user experience to breathe.</li>
+                  <li><span className="annotation">B</span>The CTA for payment process is now clearly visible fo the new user flow.</li>
+                  <li><span className="annotation">C</span>Allow users to save their credit or debit card information securely for future use. This eliminates the need for manual entry each time they want to pay a bill.</li>
+                  <li><span className="annotation">D</span>Users can now make it easier to locate and change their current data plan, ultimately improving their overall experience with your service.</li>
+                  <li><span className="annotation">E</span>Place a clearly identifiable profile icon or button in a fixed position, such as the top-right corner of the app interface, visible on every screen.</li>
+                </ul>
+              </div>
+          </div>
+
+          <h2 className="col-span-2 section-title-h2">New Low-Fidelity Design</h2>
+
+          
+          <div className="work-container">
+            <div>
+                {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
+                {/* <h2 className="section-title-h2">Low Fidelity Design</h2> */}
+                {/* <p className="body-text-p1">Together, these colors create a balanced yet dynamic palette that communicates trust and professionalism (through blue tones) and boldness or passion (through red). This combination reflects qualities often associated with space exploration: intelligence, reliability, and the adventurous spirit of discovery.</p> */}
+                <ul className="item-list">
                   <li><span className="annotation">A</span>The usage page is too cluttered, causing users to get lost and end up only checking data usage instead of using the app as a straightforward platform for making payments.</li>
-                  <li><span className="annotation">B</span>The payment gateway is hard to find because it requires navigating through the Billing section.</li>
+                  <li><span className="annotation">B</span>The CTA for payment process is now clearly visible for the new user flow.</li>
+                  <li><span className="annotation">C</span>Allow users to save their credit or debit card information securely for future use. This eliminates the need for manual entry each time they want to pay a bill.</li>
+                  {/* 
+                  <li><span className="annotation">D</span>Users often find it challenging to locate the option to change their current data plan.</li>
+                  <li><span className="annotation">E</span>The profile page is difficult to locate. Users have to explore or click all the CTA of the whole app  to find the profile settings.</li> */}
+                </ul>
+              </div>
+          </div>
+          <div>
+            <img src="./images/fido/new-screen-set001.jpg" alt="" />
+          </div>
+
+          <div className="work-container">
+            <div>
+                {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
+                {/* <h2 className="section-title-h2">Low Fidelity Design</h2> */}
+                {/* <p className="body-text-p1">Together, these colors create a balanced yet dynamic palette that communicates trust and professionalism (through blue tones) and boldness or passion (through red). This combination reflects qualities often associated with space exploration: intelligence, reliability, and the adventurous spirit of discovery.</p> */}
+                <ul className="item-list">
+                  <li><span className="annotation">D</span>Users can now make it easier to locate and change their current data plan or phone plan, ultimately improving their overall experience.</li>
+                  <li><span className="annotation">E</span>Place a clearly identifiable profile icon or button in a fixed position, such as the top-right corner of the app interface, visible on every screen.</li>
                   {/* <li><span className="annotation">C</span>Existing users always have to manually enter their credit or debit card information before paying the bill.</li>
                   <li><span className="annotation">D</span>Users often find it challenging to locate the option to change their current data plan.</li>
                   <li><span className="annotation">E</span>The profile page is difficult to locate. Users have to explore or click all the CTA of the whole app  to find the profile settings.</li> */}
                 </ul>
               </div>
+          </div>
+          <div>
+            <img src="./images/fido/new-screen-set002.jpg" alt="" />
           </div>
           
         </div>

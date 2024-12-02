3e82abb446b2ec308e25d9dde46186e278c6ac6a
Luis Baptista <>
2024-12-01 20:05:28 -0800

    Added budgetbuddy project

---
+++ src/pages/budgetbuddy.jsx
@@ -0,0 +1,21 @@
+import Hero from "../sections/bb/hero";
+import Work from "../sections/bb/work";
+import TechStack from "../sections/bb/techStack";
+import Wireframes from "../sections/bb/wireframes";
+import Parallax from '../sections/bb/parallax';
+
+import '../sections/bb/styles.css';
+
+export const Nasa = () => {
+    return (
+        <>
+            <Hero />
+            <Work />
+            <TechStack />
+            <Wireframes />
+            <Parallax />
+        </>
+    )
+  }
+  
+export default Nasa;
---
+++ src/sections/bb/hero.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+export const Hero = () => {
+  return (
+    <section className="w-full hero-section inner">
+      <div className="template-container">
+        <div>
+            <img src="./images/nasa/logo.png" alt="" />
+            <h2 className="section-title-h2 inner">UI APP Design Study</h2>
+            <p className="body-text-p1">Redesigning the NASA app aims to update its interface to better meet user needs, focusing on improving how users navigate and interact with graphics and the user interface. The goal is to create a more intuitive and engaging experience for accessing NASA’s scientific and educational resources.</p>
+            <p className="body-text-p3 pt-5">
+              {/* <Link className="btn-green mr-4" to="/">Prototype</Link> */}
+              {/* <Link className="btn-green" to="/">Website</Link> */}
+            </p>
+        </div>
+        <div>
+          <img src="./images/nasa/hero-image.png" alt="" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero;

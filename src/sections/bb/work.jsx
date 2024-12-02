73ed77a9af85449f3703f658fe5ddc8ca70e7858
Luis Baptista <>
2024-12-01 20:39:07 -0800

    Added introduction section

--- src/sections/bb/work.jsx
+++ src/sections/bb/work.jsx
@@ -2,64 +2,94 @@ export const Work = () => {
     return (
       <section className="w-full">
         <div className="work-section">
-            <h2 className="col-span-2 section-title-h2">UI Design Study</h2>
+          <h2 className="col-span-2 section-title-h2">Introduction</h2>
 
-            <div className="block-container wgrid">
-              <div>
-                  <img src="./images/nasa/colors.png" alt="" />
-              </div>
-              <div className="work-container">
-                  <div>
-                    {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
-                    <h3 className="section-title-h3">Color Usage</h3>
-                    <p className="body-text-p1">Together, these colors create a balanced yet dynamic palette that communicates trust and professionalism (through blue tones) and boldness or passion (through red). This combination reflects qualities often associated with space exploration: intelligence, reliability, and the adventurous spirit of discovery.</p>
-                  </div>
-              </div>
+          <div className="block-container ngrid">
+            <div className="work-container">
+                <div className="temp-position-3 lg:grid">
+                <div>
+                  {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
+                  <h3 className="section-title-h3">Track Expenses</h3>
+                  <p className="body-text-p1">Effortlessly track your expenses on-the-go so you can manage them effectively.</p>
+                </div>
+
+                <div>
+                  {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
+                  <h3 className="section-title-h3">Budget Management</h3>
+                  <p className="body-text-p1">Effectively manage your budget and expenses and get proper insights.</p>
+                </div>
+
+                <div>
+                  {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
+                  <h3 className="section-title-h3">Goal Management</h3>
+                  <p className="body-text-p1">Easily set up, manage and achieve your financial goals.</p>
+                </div>
+                </div>
+
+                <div className="ss-images gallery-bg p-10">
+                <img src="./images/bb/features.png" alt="" />
+                </div>
+            </div>
+          </div>
+
+          <h2 className="col-span-2 section-title-h2">UI Design Study</h2>
+
+          <div className="block-container wgrid">
+            <div>
+                <img src="./images/nasa/colors.png" alt="" />
+            </div>
+            <div className="work-container">
+                <div>
+                  {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
+                  <h3 className="section-title-h3">Color Usage</h3>
+                  <p className="body-text-p1">Together, these colors create a balanced yet dynamic palette that communicates trust and professionalism (through blue tones) and boldness or passion (through red). This combination reflects qualities often associated with space exploration: intelligence, reliability, and the adventurous spirit of discovery.</p>
+                </div>
+            </div>
+          </div>
+
+          <div className="block-container ngrid">
+            <div className="work-container">
+                <div>
+                  {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
+                  <h3 className="section-title-h3">Iconography</h3>
+                  <p className="body-text-p1">These icons would look striking in the selected colors, with navy and dark blue for backgrounds or bases and the red for highlights, giving them a sharp, cohesive look that reinforces the space exploration theme.</p>
+                </div>
+
+                <div className="ss-images temp-position-2">
+                  <img src="./images/nasa/icon001.png" alt="" />
+                  <img src="./images/nasa/icon002.png" alt="" />
+                </div>
+            </div>
+          </div>
+          
+
+          <div className="block-container ngrid">
+            <div className="work-container">
+                <div>
+                  {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
+                  <h3 className="section-title-h3">Design Kit</h3>
+                  <p className="body-text-p1">These icons would look striking in the selected colors, with navy and dark blue for backgrounds or bases and the red for highlights, giving them a sharp, cohesive look that reinforces the space exploration theme.</p>
+                </div>
+
+                <div className="ss-images temp-position-3">
+                  <img src="./images/nasa/comp001.png" alt="" />
+                  <img src="./images/nasa/comp002.png" alt="" />
+                  <img src="./images/nasa/comp003.png" alt="" />
+                </div>
+            </div>
+          </div>
+          
+          <div className="block-container wgrid">
+            <div className="work-container">
+                  {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
+                  <h3 className="section-title-h3">Typography</h3>
+                  <p className="body-text-p1">Choosing Noto Sans as the typeface offers several benefits. It’s designed for easy reading across different languages and platforms, ensuring clarity and accessibility for diverse users. Noto Sans supports a wide range of characters and scripts, maintaining a consistent appearance across various devices. Being part of the open-source Google Noto font family makes it cost effective and widely accessible for projects with different budget constraints. Overall, Noto Sans is a practical chose for projects needing clear communication and readability across a global audience.</p>
             </div>
 
-            <div className="block-container ngrid">
-              <div className="work-container">
-                  <div>
-                    {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
-                    <h3 className="section-title-h3">Iconography</h3>
-                    <p className="body-text-p1">These icons would look striking in the selected colors, with navy and dark blue for backgrounds or bases and the red for highlights, giving them a sharp, cohesive look that reinforces the space exploration theme.</p>
-                  </div>
-
-                  <div className="ss-images temp-position-2">
-                    <img src="./images/nasa/icon001.png" alt="" />
-                    <img src="./images/nasa/icon002.png" alt="" />
-                  </div>
-              </div>
+            <div>
+                <img src="./images/nasa/typo.jpg" alt="" />
             </div>
-            
-
-            <div className="block-container ngrid">
-              <div className="work-container">
-                  <div>
-                    {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
-                    <h3 className="section-title-h3">Design Kit</h3>
-                    <p className="body-text-p1">These icons would look striking in the selected colors, with navy and dark blue for backgrounds or bases and the red for highlights, giving them a sharp, cohesive look that reinforces the space exploration theme.</p>
-                  </div>
-
-                  <div className="ss-images temp-position-3">
-                    <img src="./images/nasa/comp001.png" alt="" />
-                    <img src="./images/nasa/comp002.png" alt="" />
-                    <img src="./images/nasa/comp003.png" alt="" />
-                  </div>
-              </div>
-            </div>
-            
-            <div className="block-container wgrid">
-              <div className="work-container">
-                    {/* <p className="body-text-p2"><small>Color Palette</small></p> */}
-                    <h3 className="section-title-h3">Typography</h3>
-                    <p className="body-text-p1">Choosing Noto Sans as the typeface offers several benefits. It’s designed for easy reading across different languages and platforms, ensuring clarity and accessibility for diverse users. Noto Sans supports a wide range of characters and scripts, maintaining a consistent appearance across various devices. Being part of the open-source Google Noto font family makes it cost effective and widely accessible for projects with different budget constraints. Overall, Noto Sans is a practical chose for projects needing clear communication and readability across a global audience.</p>
-              </div>
-
-              <div>
-                  <img src="./images/nasa/typo.jpg" alt="" />
-              </div>
-            </div>
+          </div>
 
 
         </div>
